675862cb5f6ad75bd7bd16ba524d8e9167ad737a
Paweł Pisarek <pisarekp73@gmail.com>
2020-10-07 23:04:35 +0200

    fix change detection

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,7 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ApplicationRef, ChangeDetectionStrategy, Component} from '@angular/core';
 import {ModalService} from '../modal.service';
 import {LoginModalComponent} from '../login-modal/login-modal.component';
-import {Observable, timer} from 'rxjs';
-import {tap} from 'rxjs/operators';
+import {BehaviorSubject, Observable, timer} from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -15,18 +14,13 @@ export class DashboardComponent {
   constructor(private modalService: ModalService<LoginModalComponent, LoginModalComponent, Observable<boolean>, { username: string }>) {
   }
 
-  login(loginForm: { username: string }): void {
+  saveFromHandler(loginForm: { username: string }): void {
     const componentRef = this.modalService.open({component: LoginModalComponent, value: loginForm});
-    // setTimeout(()=>{
-    timer(1,100).pipe(tap(aasd=>{
-      console.log(aasd);
-      componentRef.modalRef.changeDetectorRef.detectChanges();
-    }))
-    // },1);
     componentRef.afterClosed().subscribe(isLoggedIn => {
       if (isLoggedIn) {
         alert('zalogowano');
       }
     });
+    componentRef.modalRef.changeDetectorRef.detectChanges();
   }
 }
--- src/app/modal-wrapper/modal-wrapper.component.ts
+++ src/app/modal-wrapper/modal-wrapper.component.ts
@@ -1,6 +1,6 @@
-import {ChangeDetectionStrategy, Component, ComponentFactoryResolver, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
+import {ChangeDetectionStrategy, Component, ComponentFactoryResolver, OnDestroy, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
 import {ModalService} from '../modal.service';
-import {Subject} from 'rxjs';
+import {Subject, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-modal-wrapper',
@@ -8,24 +8,34 @@ import {Subject} from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./modal-wrapper.component.scss']
 })
-export class ModalWrapperComponent implements OnInit {
+export class ModalWrapperComponent implements OnInit, OnDestroy {
 
+  private subscription: Subscription;
   @ViewChild('dynamicComponentContainer', {read: ViewContainerRef}) dynamicComponentContainer;
+
   constructor(private componentFactoryResolver: ComponentFactoryResolver,
               private viewContainerRef: ViewContainerRef,
-              private modalService: ModalService<any, any, any, any>) { }
-
-  ngOnInit(): void {
-    this.modalService.modals$.subscribe(modal => {
-      const factory = this.componentFactoryResolver.resolveComponentFactory(modal.modal.component);
-      const componentRef = this.dynamicComponentContainer.createComponent(factory);
-      componentRef.changeDetectorRef.detectChanges();
-      const afterClose = new Subject();
-      componentRef.instance.destroyCallback = (value?) => { componentRef.destroy(); afterClose.next(value); };
-      componentRef.instance.value = modal.modal.value;
-      modal.modalRefs$.next({modalRef: componentRef, afterClosed: () => afterClose});
-      componentRef.changeDetectorRef.detectChanges();
-    });
+              private modalService: ModalService<any, any, any, any>) {
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    this.subscription = this.modalService.modals$.subscribe(modal => {
+      const factory = this.componentFactoryResolver.resolveComponentFactory(modal.modal.component);
+      const componentRef = this.dynamicComponentContainer.createComponent(factory);
+      const afterClose = new Subject();
+      componentRef.instance.destroyCallback = (value?) => {
+        componentRef.destroy();
+        afterClose.next(value);
+      };
+      componentRef.instance.value = modal.modal.value;
+      modal.modalRefs$.next({
+        modalRef: componentRef, afterClosed: () => afterClose
+      });
+      componentRef.changeDetectorRef.markForCheck();
+    });
+  }
 }
